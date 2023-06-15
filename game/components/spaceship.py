cc56d464c2930d3a5e4435f3881c27157567b9ce
ValFJ2023 <valentina.betancur@fundacion-jala.org>
2023-06-14 19:04:32 -0500

    Clase 1: Spaceship tarea

--- game/components/spaceship.py
+++ game/components/spaceship.py
@@ -4,12 +4,19 @@ from pygame.sprite import Sprite
 from game.utils.constants import SPACESHIP, SCREEN_HEIGHT, SCREEN_WIDTH
 
 
-class Spaceship:
+class Spaceship(Sprite):
+    SHIP_WIDTH = 40
+    SHIP_HEIGHT = 60
+    X_POS = (SCREEN_WIDTH // 2) - SHIP_WIDTH
+    Y_POS = 500
+    SHIP_SPEED = 10
+
     def __init__(self):
+        self.image= SPACESHIP
         self.image = pygame.transform.scale(SPACESHIP,(60,40))
         self.rect = self.image.get_rect()
-        self.rect.x = 520
-        self.rect.y = 500
+        self.rect.x = self.X_POS
+        self.rect.y = self.Y_POS
 
     def update(self, user_input):
         if user_input[pygame.K_LEFT]:
@@ -24,23 +31,22 @@ class Spaceship:
             self.move_down()
 
     def move_left(self):
-        if self.rect.left > 0:
-            self.rect.x -= 10
-        
+        self.rect.x -= self.SHIP_SPEED
+        if self.rect.right < 0:
+            self.rect.left = SCREEN_WIDTH
     
     def move_right(self):
-        if self.rect.left < SCREEN_WIDTH:
-            self.rect.x += 10
-
+        self.rect.x += self.SHIP_SPEED
+        if self.rect.left > SCREEN_WIDTH:
+            self.rect.right = 0
     
     def move_up(self):
-        if self.rect.left > SCREEN_HEIGHT // 2:
-            self.rect.y -= 10
-
+        if self.rect.y > SCREEN_HEIGHT // 2:
+            self.rect.y -= self.SHIP_SPEED
     
     def move_down(self):
-        if self.rect.left < SCREEN_HEIGHT - 50:
-            self.rect.y += 10
+        if self.rect.y < SCREEN_HEIGHT - 70:
+            self.rect.y += self.SHIP_SPEED
    
             
 
